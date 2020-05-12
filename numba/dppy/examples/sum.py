977e63cf2b3a8d78b76cb3264f8f761aa6ea486f
Diptorup Deb <diptorup.deb@intel.com>
2020-05-12 17:49:52 -0500

    Update sum.py to use dppy context manager.

--- numba/dppy/examples/sum.py
+++ numba/dppy/examples/sum.py
@@ -5,7 +5,7 @@ from timeit import default_timer as time
 import sys
 import numpy as np
 from numba import dppy
-import dppy.core as ocldrv
+import dppy as ocldrv
 
 
 @dppy.kernel
@@ -23,32 +23,18 @@ def main():
     b = np.array(np.random.random(N), dtype=np.float32)
     c = np.ones_like(a)
 
-    # Select a device for executing the kernel
-    device_env = None
+    with ocldrv.igpu_context() as device_env:
+        # Copy the data to the device
+        dA = device_env.copy_array_to_device(a)
+        dB = device_env.copy_array_to_device(b)
+        dC = ocldrv.DeviceArray(device_env.get_env_ptr(), c)
 
-    try:
-        device_env = ocldrv.runtime.get_gpu_device()
-        print("Selected GPU device")
-    except:
-        try:
-            device_env = ocldrv.runtime.get_cpu_device()
-            print("Selected CPU device")
-        except:
-            print("No OpenCL devices found on the system")
-            raise SystemExit()
-
-
-    # Copy the data to the device
-    dA = device_env.copy_array_to_device(a)
-    dB = device_env.copy_array_to_device(b)
-    dC = ocldrv.DeviceArray(device_env.get_env_ptr(), c)
-
-    print("before : ", dA._ndarray)
-    print("before : ", dB._ndarray)
-    print("before : ", dC._ndarray)
-    data_parallel_sum[device_env, global_size](dA, dB, dC)
-    device_env.copy_array_from_device(dC)
-    print("after : ", dC._ndarray)
+        print("before : ", dA._ndarray)
+        print("before : ", dB._ndarray)
+        print("before : ", dC._ndarray)
+        data_parallel_sum[device_env, global_size](dA, dB, dC)
+        device_env.copy_array_from_device(dC)
+        print("after : ", dC._ndarray)
 
     print("Done...")
 
